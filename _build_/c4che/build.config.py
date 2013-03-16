c1b25bec62fb06a466f5cce1599c6c676875ec00
jcnoir <jcnoir@free.fr>
2013-03-16 16:16:47 +0100

    commit regulier

--- _build_/c4che/build.config.py
+++
@@ -1,2 +0,0 @@
-version = 0x10512
-tools = [{'tooldir': None, 'funs': None, 'tool': 'ar'}, {'tooldir': None, 'funs': None, 'tool': 'cc'}, {'tooldir': None, 'funs': None, 'tool': 'gcc'}, {'tooldir': None, 'funs': None, 'tool': 'compiler_cc'}, {'tooldir': None, 'funs': None, 'tool': 'gnu_dirs'}, {'tooldir': None, 'funs': None, 'tool': 'vala'}]
